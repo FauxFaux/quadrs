473a3229c7b5561e059a97f455fa6377983561d7
Chris West (Faux) <git@goeswhere.com>
2018-01-30 21:42:28 +0000

    I think it decodes properly, but the data is junk

--- src/bits.rs
+++ src/bits.rs
@@ -1,3 +1,60 @@
+use usize_from;
+
+fn decode(data: &[bool]) -> String {
+    let mut data = data.into_iter().cloned();
+    let mut s = String::with_capacity(data.len() / 8);
+    'a: loop {
+        let mut val = 0u8;
+        for bit in (0..8).rev() {
+            match data.next() {
+                Some(true) => val |= (1 << bit),
+                Some(false) => {}
+                None => break 'a,
+            }
+        }
+        s.push(val as char);
+    }
+
+    s
+}
+
+fn scan(data: &[bool], scale: f64) -> (f64, Vec<bool>) {
+    let mut i = 0;
+    let half = usize_from((scale / 2.).round() as u64);
+    let mut bit = false;
+    let mut error = 0.;
+    let mut ret = Vec::with_capacity(data.len() / usize_from((scale + 2.) as u64));
+    while i != data.len() {
+        let found = run_of(&data[i..], half, bit);
+        i += found;
+
+        if found <= half {
+            continue;
+        }
+
+        let bits = (found as f64) / scale;
+
+        #[cfg(never)]
+        println!(
+            "{:0.2} {} {}",
+            bits,
+            if bit { "X" } else { "." },
+            fmt(&data[(i - found)..])
+        );
+
+        let rounded = bits.round();
+        error += (bits - rounded).abs();
+
+        for _ in 0..(rounded as u64) {
+            ret.push(bit);
+        }
+
+        bit = !bit;
+    }
+
+    (error, ret)
+}
+
 fn run_of(data: &[bool], scale: usize, val: bool) -> usize {
     let mut bad = 0;
     for i in 0..data.len() {
@@ -31,6 +88,10 @@ mod tests {
 
     #[test]
     fn de_run() {
+        use super::decode;
+        use super::scan;
+
+        // 1100000110010100110000011111110100101100011101010001000001000001
         let inp = parse(
             r#"11
                  010000000111110000011111000001111100001111100000111110000011111000001111100001
@@ -43,6 +104,17 @@ mod tests {
                  000001111100000000000000011111000000000000000000000000111110000000000000000000
                  00000111111110101"#,
         );
+
+        let (err, val) = scan(&inp, 4.8);
+        println!("{}", super::fmt(&val));
+        for off in 0..8 {
+            println!("{:?}", decode(&val[off..]));
+        }
+
+        let inv: Vec<bool> = val.iter().map(|&x| !x).collect();
+        for off in 0..8 {
+            println!("{:?}", decode(&inv[off..]));
+        }
     }
 
     fn parse(s: &str) -> Vec<bool> {
@@ -55,4 +127,9 @@ mod tests {
             })
             .collect()
     }
+
+}
+
+fn fmt(bits: &[bool]) -> String {
+    bits.iter().map(|&x| if x { 'X' } else { '.' }).collect()
 }
