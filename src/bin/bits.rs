e01ec4d7f0261d6d99f7b2c33409bb842247fbe3
Chris West (Faux) <chris@tessl.io>
2025-07-07 19:11:13 +0100

    wip

--- src/bin/bits.rs
+++ src/bin/bits.rs
@@ -1,5 +1,6 @@
 use std::{fs, io};
 use anyhow::{bail, Result};
+use quadrs::eui::display;
 
 fn main() -> Result<()> {
     for (li, line) in io::stdin().lines().enumerate() {
@@ -27,23 +28,32 @@ fn main() -> Result<()> {
             }
         }
 
-        println!("line: {line}");
+        println!("line ({li}): {line}");
 
         let key = b"GROWATTRF.";
 
         for i_skip in 0..8 {
             let chars = bits[i_skip..].chunks_exact(8).map(to_byte).map(char::from).collect::<String>();
-            println!("decode {i_skip}: {chars:?}");
+            let chars = match chars.find("jack") {
+                Some(i) if i > 4 && i < 10 => chars[(i+4)..].to_string(),
+                _ => continue,
+            };
+            println!("decode {i_skip}: {}", unambiguous(chars.as_bytes()));
+            println!(" - R: {}", printables(chars.as_bytes()));
+            
             for i_key in 0..key.len() {
-                let decoded = chars.bytes().zip(key.iter().cycle().skip(i_key)).map(|(a, b)| a ^ *b).collect::<Vec<u8>>();
-                let display = decoded.iter()
-                    .map(|&b| if b.is_ascii_graphic() { b } else if b == 0 { b' ' } else { b'.' })
-                    .map(char::from).collect::<String>();
-                println!(" - {i_key}: {display:?}");
+                let proposed_key = || key.iter().cycle().skip(i_key);
+                let decoded = chars.bytes().zip(proposed_key()).map(|(a, b)| a ^ *b).collect::<Vec<u8>>();
+                let display = printables(decoded.as_slice());
+                println!(" - {i_key}: {display}");
 
-                if decoded.chunks_exact(10).any(|chunk| chunk.iter().all(|c| c.is_ascii_uppercase() || c.is_ascii_digit())) {
+                if decoded.chunks_exact(10).any(|chunk| chunk.iter().all(|c| *c==0) || chunk.iter().all(|c| c.is_ascii_uppercase() || c.is_ascii_digit())) {
                     println!("  - found key at {i_key}");
-                    println!("  - decoded: {decoded:?}");
+                    println!(" - K: {}", proposed_key().take(decoded.len())
+                        .cloned()
+                        .map(char::from)
+                        .collect::<String>());
+                    // println!("  - decoded: {decoded:?}");
                     fs::write(format!("decoded_{li}_{i_skip}_{i_key}.bin"), decoded)?;
                 }
             }
@@ -54,6 +64,24 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn unambiguous(decoded: impl AsRef<[u8]>) -> String {
+    let mut buf = String::with_capacity(2 * decoded.as_ref().len());
+    for c in decoded.as_ref() {
+        if c.is_ascii_alphabetic() {
+            buf.push(char::from(*c));
+        } else {
+            buf.push_str(&format!("\\d{{{}}}", c));
+        }
+    }
+    buf
+}
+
+fn printables(decoded: impl AsRef<[u8]>) -> String {
+    decoded.as_ref().iter()
+        .map(|&b| if b.is_ascii_graphic() { b } else if b == 0 { b' ' } else { b'.' })
+        .map(char::from).collect::<String>()
 }
 
 fn to_byte(bits: &[u8]) -> u8 {
